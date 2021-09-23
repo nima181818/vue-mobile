801a82e7d784f9d3b403d4d334413e873c98f4ce
wangtao <601028413@qq.com>
2021-09-23 09:09:40 +0800

    init options

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      active:0
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+ 
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import vant from 'vant'
+import 'vant/lib/index.css'
+import './rem.js'
+Vue.config.productionTip = false
+Vue.use(vant)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
